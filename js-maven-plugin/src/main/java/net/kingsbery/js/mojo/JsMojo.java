21973a5c89486f90c015e3cd2ac63e4691b45743
James Kingsbery <06jameskingsbery@gmail.com>
2011-02-11 00:25:33 -0500

    Added most basic functionality to mojo. To implement properly will require some refactoring.

--- js-maven-plugin/src/main/java/net/kingsbery/js/mojo/JsMojo.java
+++ js-maven-plugin/src/main/java/net/kingsbery/js/mojo/JsMojo.java
@@ -1,17 +1,57 @@
 package net.kingsbery.js.mojo;
 
+import java.io.File;
+import java.io.IOException;
+
+import net.kingsbery.js.metrics.RulesCompliance;
+import net.kingsbery.js.metrics.RulesCompliance.RulesComplianceResult;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 
 
 /**
- * Says "Hi" to the user.
- * @goal sayhi
+ * Compiles stats on the project's JavaScript code.
+ * 
+ * @goal stat
  */
 public class JsMojo extends AbstractMojo
 {
-    public void execute() throws MojoExecutionException
+    
+    /**
+     * The directory that contains the main JavaScript code.
+     * @parameter
+     */
+    private File mainJs;
+    
+    /**
+     * @parameter
+     */
+    private File testJs;
+    
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException
     {
-        getLog().info("Hello, world.");
+        RulesComplianceResult result;
+        try {
+            result = new RulesCompliance().process(mainJs);
+            System.out.println(result.getTotalLinesOfCode());
+            System.out.println(result.getTotalViolations());
+            System.out.println(result.getCompliance());
+        } catch (IOException e) {
+            getLog().error(e);
+            throw new MojoFailureException("There was a problem getting JavaScript stats");
+        }
+    }
+    
+    public void setMainCode(File file){
+        this.mainJs=file;
+    }
+    
+    public static void main(String args[]) throws MojoExecutionException, MojoFailureException{
+        JsMojo mojo = new JsMojo();
+        mojo.setMainCode(new File("../js-quality-core/src/test/resources"));
+        mojo.execute();
     }
 }
